d3becd5fb37906960904cb1bab565a3cd0870272
vikki <vchwalowski@gmail.com>
2013-09-26 20:44:36 +0000

    added logging and proper emailing and other things

--- ipreport.sh
+++ ipreport.sh
@@ -5,14 +5,28 @@ if [ -z "$1" ]; then
   exit 1
 fi
 
-FILE="currentip"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+FILE="$DIR/currentip"
 HOSTNAME=`hostname`
 EMAIL=$1
+LOGLEVEL=2 # 0 = none, 1 = changes, 2 = all
+LOGFILE="$DIR/log"
+NOW=$(date +"%Y/%m/%d %T")
 IP=`wget -q -O - checkip.net-me.net/plain`
+
+if [[ $LOGGING -ge 2 ]]; then
+  echo "$NOW : running"
+fi
 
 if [[ ! -e $FILE ]]; then
   echo $IP > $FILE
+  if [[ $LOGGING -ge 1 ]]; then
+    echo "$NOW : made new file $FILE"
+  fi
 elif [ '$(head -n 1 $FILE)' != $IP ]; then
   echo $IP > $FILE
-  mail -s "$HOSTNAME has a new IP!" vchwalowski@gmail.com <<< $FILE
+  mailx -s "$HOSTNAME has a new IP!" $EMAIL <<< $FILE
+  if [[ LOGGING -ge 1 ]]; then
+    echo "$NOW : ip changed to $IP, sent email"
+  fi
 fi
